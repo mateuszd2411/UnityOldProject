2cc50d18fff782f9a24f7a7f41257b1fb99ed3a6
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-14 20:26:22 +0200

    2D pratform unity part2

---
+++ 2D Platform Game/2d platform 2-3/2DPlatform/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster gm;
+    
+
+    private void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+    public Transform spawnPrefab;
+
+    public IEnumerator RespawnPlayer ()
+    {
+
+        GetComponent<AudioSource>().Play ();
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Transform clone = (Transform)Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation);
+        Destroy(clone.gameObject, 3f);
+    }
+
+    public static void KillPlayer (Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+    }
+
+    public static void KillEnemy (Enemy enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+}
---
+++ 2D Platform Game/2d platform 2-3/2DPlatform/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [System.Serializable]
+    public class PlayerStats
+    {
+        public int Health = 100;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int fallBoundary = -20;
+
+    private void Update()
+    {
+        if (transform.position.y <= fallBoundary)
+            DamagePlayer(99999999);
+    }
+
+    public void DamagePlayer (int damage)
+    {
+        playerStats.Health -= damage;
+        if (playerStats.Health <= 0)
+        {
+            GameMaster.KillPlayer(this);
+        }
+    }	
+}
