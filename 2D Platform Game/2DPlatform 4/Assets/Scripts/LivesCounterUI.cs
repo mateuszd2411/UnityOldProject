019b61fe53d078ff8c5a2910cfaae8a771120d48
mateuszd2411 <mateuszd2411@gmail.com>
2020-07-14 20:36:57 +0200

    2D pratform 4 unity

---
+++ 2D Platform Game/2DPlatform 4/Assets/Scripts/LivesCounterUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Text))]
+public class LivesCounterUI : MonoBehaviour {
+
+    private Text livesText;
+
+	void Awake () {
+        livesText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        livesText.text = "LIVES: " + GameMaster.RemainingLives.ToString();
+	}
+}
---
+++ 2D Platform Game/2DPlatform 4/Assets/Scripts/MenuMenager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMenager : MonoBehaviour {
+
+    [SerializeField]
+    string hoverOverSound = "ButtonHover";
+
+    [SerializeField]
+    string pressButtonSound = "ButtonPress";
+
+    AudioManager audioManager;
+
+    private void Start()
+    {
+        audioManager = AudioManager.instance;
+        if (audioManager == null)
+        {
+            Debug.LogError("No audiomanager found");
+        }
+    }
+
+    public void StartGame ()
+    {
+        audioManager.PlaySound(pressButtonSound);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void QuitGame()
+    {
+        audioManager.PlaySound(pressButtonSound);
+        Debug.Log("We Quit the Game");
+        Application.Quit();
+    }
+
+    public void OnMouseOver()
+    {
+        audioManager.PlaySound(hoverOverSound);
+    }
+
+}
